59f2381dea3c60c9361095b05c99deb1fc2b73e0
Furkan Dize <dizefurkan@gmail.com>
2017-12-25 14:51:36 +0300

    Added active class and fixed fade in-out bugs on Portfolio

--- _assets/js/portfolio.js
+++ _assets/js/portfolio.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
   $('.portfolio__button').on("click", function() {
+    $('.portfolio__button').removeClass("active");
+    $(this).addClass("active");
     var filter = $(this).data();
     $.each(filter, function(key,value) {
       var controlKey = $('.portfolio__list').find('.' + value).length;
 
       if (controlKey) {
-        $('.portfolio__list').find('.list__item').fadeOut();
-        $('.portfolio__list').find('.' + value).fadeIn();
-      } else {
-        $('.portfolio__list').find('.list__item').fadeOut();
-      }
+        $('.portfolio__list').find('.list__item').fadeOut('fast');
+        setTimeout(function() {
+          $('.portfolio__list').find('.' + value).fadeIn('slow');
+        },500);
+      } 
+      // else {
+      //   $('.portfolio__list').find('.list__item').fadeOut('slow');
+      // }
     });
   });
 });
